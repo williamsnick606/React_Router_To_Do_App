b875ca0dffe5e8ad1ee52c2d75231d1e84543fe3
Nicholas Williams <williamsnick606@gmail.com>
2023-08-03 00:43:42 -0700

    Commiting React Router todo App to GitHub

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
-function App() {
+import Layout from './components/Layout';
+import Home from './components/Home';
+import About from './components/About';
+import Posts from './components/Posts';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	<Router>
+		<Routes>
+			<Route path="/" element={<Layout />}>
+				<Route index element={<Home />} />
+				<Route path="about" element={<About />} />
+				<Route path="posts" element={<Posts />} />
+			</Route>
+		</Routes>
+	</Router>
   );
-}
+};
 
 export default App;
